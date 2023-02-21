ca917328721503f117628599911d436930d63605
Dmitrii <elfin85@gmail.com>
2023-02-21 18:12:37 +0100

    refactor css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Skills from "./components/skills/Skills";
+import Works from "./components/works/Works";
+import Contact from "./components/contacts/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Works/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
